94526b7c88fb7281c8621405984eadf841000d55
HananINouman <hanan@obol.tech>
2023-07-25 19:23:28 +0300

    add ts, js eexamples

---
+++ JS-Example/index.js
@@ -0,0 +1,42 @@
+import obolSDK from "@obolnetwork/obol-sdk";
+import { ethers } from "ethers";
+
+/**
+ * @returns instantiates and returns Obol SDK Client
+ */
+const obolClient = async () => {
+  const provider = new ethers.BrowserProvider(window.ethereum);
+  const signer = await provider.getSigner();
+  const client = new obolSDK.Client({ baseUrl: "https://api.obol.tech", chainId: 5 }, signer);
+  return client
+}
+
+/**
+ * Returns the cluster config hash after saving cluster definition
+ * @param cluster The cluster defintion { name:string, operators:{address:string}[], validators:{fee_recipient_address:string,withdrawal_address:string}[]}
+ * @returns The config hash
+ */
+const createObolCluster = async (clusterConfig) => {
+  try {
+    const client = await obolClient();
+    const configHash = await client.createClusterDefinition(clusterConfig);
+    return configHash;
+  } catch (err) {
+    console.log(err, "err");
+  }
+};
+
+/**
+ * Returns the cluster lock
+ * @param configHash The cluster hash returned from createClusterDefinition
+ * @returns The cluster lock
+ */
+const getObolClusterLock = async (configHash) => {
+  try {
+    const client = await obolClient();
+    const lockFile = await client.getClusterLock(configHash);
+    return lockFile;
+  } catch (err) {
+    console.log(err, "err");
+  }
+};
---
+++ TS-Example/index.ts
@@ -0,0 +1,45 @@
+import { ethers } from "ethers";
+import obolSDK from "@obolnetwork/obol-sdk";
+import { ClusterLock } from "@obolnetwork/obol-sdk/dist/types";
+
+
+/**
+ * @returns instantiates and returns Obol SDK Client
+ */
+const obolClient = async (): Promise<obolSDK.Client> => {
+  const provider = new ethers.BrowserProvider((window as any).ethereum);
+  const signer = await provider.getSigner();
+  const client = new obolSDK.Client({ baseUrl: "https://api.obol.tech", chainId: 5 }, signer);
+  return client
+}
+
+
+/**
+ * Returns the cluster config hash after saving cluster definition
+ * @param cluster The cluster defintion
+ * @returns The config hash
+ */
+const createObolCluster = async (clusterConfig: { name: string, operators: { address: string }[], validators: { fee_recipient_address: string, withdrawal_address: string }[] }): Promise<string> => {
+  try {
+    const client = await obolClient();
+    const configHash = await client.createClusterDefinition(clusterConfig);
+    return configHash;
+  } catch (err) {
+    console.log(err, "err");
+  }
+};
+
+/**
+ * Returns the cluster lock
+ * @param configHash The cluster hash returned from createClusterDefinition
+ * @returns The cluster lock
+ */
+const getObolClusterLock = async (configHash: string): Promise<ClusterLock> => {
+  try {
+    const client = await obolClient();
+    const lockFile = await client.getClusterLock(configHash);
+    return lockFile;
+  } catch (err) {
+    console.log(err, "err");
+  }
+};
